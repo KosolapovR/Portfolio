969390c37768cdd2200732a2e1eb1fb7c71155cf
Kosolapov Roman <Kosolapov-r@bk.ru>
2019-08-07 08:17:40 +0300

    start project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content= "width=device-width, initial-scale=1.0"> 
+    <meta http-equiv="Cache-Control" content="public">
+    <meta name="description" content="">
+    <title>Интернет магазин систем видеонаблудения, купить системы видеонаблюдения, VOIP</title>
+    <link rel="shortcut icon" href="" type="image/png">
+    <title>My portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+   <div class="header_container">
+    <nav>
+      <ul class="menu">
+          <li>home</li>
+          <li>works</li>
+          <li>skills</li>
+          <li>contact</li>
+      </ul>
+    </nav>
+    <div class="title">
+           <p class="name"></p>
+           <h1>Web Developer</h1>
+           <div class="title-btn title_btn-work"></div>
+           <div class="title-btn title_btn-hire"></div>
+       </div>
+    </div>
+  </header>
+</body>
+</html>
