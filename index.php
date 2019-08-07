aefce2fb276dd4d2e39ab4b17d15185d94817dcd
Kosolapov Roman <Kosolapov-r@bk.ru>
2019-08-07 12:20:37 +0300

    header

--- index.php
+++ index.php
@@ -15,19 +15,100 @@
    <div class="header_container">
     <nav>
       <ul class="menu">
-          <li>home</li>
-          <li>works</li>
-          <li>skills</li>
-          <li>contact</li>
+          <li><a href=""></a>home</li>
+          <li><a href=""></a>works</li>
+          <li><a href=""></a>skills</li>
+          <li><a href=""></a>contact</li>
       </ul>
     </nav>
     <div class="title">
-           <p class="name"></p>
+           <p class="name">Kosolapov Roman</p>
            <h1>Web Developer</h1>
-           <div class="title-btn title_btn-work"></div>
-           <div class="title-btn title_btn-hire"></div>
+           <div class="title_btns">
+             <div class="title-btn title_btn-work">view work</div>
+             <div class="title-btn title_btn-hire">hire me</div>
+           </div>
+           <div class="mouse">
+               <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 210.414 210.414" style="enable-background:new 0 0 210.414 210.414;" xml:space="preserve">
+		<g>
+			<path d="M105.207,0C66.532,0,35.069,31.45,35.069,70.104v70.21c0,38.653,31.463,70.1,70.138,70.1
+				c38.675,0,70.138-31.446,70.138-70.1v-70.21C175.345,31.45,143.881,0,105.207,0z M167.552,140.314
+				c0,34.357-27.968,62.307-62.345,62.307c-34.377,0-62.345-27.95-62.345-62.307v-70.21c0-34.357,27.968-62.311,62.345-62.311
+				c34.377,0,62.345,27.953,62.345,62.311V140.314z"/>
+			<path d="M105.207,23.379c-2.152,0-3.897,1.743-3.897,3.897v35.286c0,2.154,1.745,3.897,3.897,3.897
+				c2.152,0,3.897-1.743,3.897-3.897V27.276C109.103,25.122,107.359,23.379,105.207,23.379z"/>
+		</g>
+</svg>
+           </div>
        </div>
     </div>
   </header>
+  <main>
+    <section class="advantage advantage_card1">
+      <div class="advantage_card">
+          <div class="advantage_card_icon"><img src="icons/transfer.svg" alt="responsive_icon"></div>
+          <h4 class="advantage_card_title">Отзывчивая верстка</h4>
+          <p class="advantage_card_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, assumenda.</p>
+     </div>
+      <div class="advantage_card advantage_card2">
+          <div class="advantage_card_icon"><img src="icons/responsive-design-symbol.svg" alt="adaptive_icon"></div>
+          <h4 class="advantage_card_title">Адаптивная верстка</h4>
+          <p class="advantage_card_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, assumenda.</p>
+      </div>
+      <div class="advantage_card advantage_card3">
+          <div class="advantage_card_icon"><img src="icons/web.svg" alt="web.icon"></div>
+          <h4 class="advantage_card_title">
+          Кроссбраузерная верстка
+          </h4>
+          <p class="advantage_card_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, assumenda.</p>
+      </div>
+      <div class="advantage_card advantage_card4">
+          <div class="advantage_card_icon"><img src="icons/javascript-hand-drawn-file.svg" alt="js_icon"></div>
+          <h4 class="advantage_card_title">
+          Интеграция JavaScript
+          </h4>
+          <p class="advantage_card_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, assumenda.</p>
+      </div>
+    </section>  
+   
+    <section class="about">
+        <h2>обо мне</h2>
+        <div class="about_container">
+            <div class="info">
+                <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat fuga culpa sed fugit sapiente quaerat harum reprehenderit recusandae iusto mollitia!</div>
+                <table>
+                    <tr>
+                        <td class="col1">Name</td>
+                        <td><span>:</span>Kosolapov Roman</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Email</td>
+                        <td><span>:</span>Kosolapov-r@bk.ru</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Phone</td>
+                        <td><span>:</span>+7 (951) 659-17-52</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Date of Birth</td>
+                        <td><span>:</span>04.06.1991</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Job</td>
+                        <td><span>:</span>Freelance</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="photo"><img src="" alt=""></div>
+            <div class="progress"></div>
+            <div class="stack"></div>
+        </div>
+    </section>
+    <section class="skills"></section>
+    <section class="works"></section>
+    <section class="contact"></section>
+  </main>
+  <footer></footer>
 </body>
 </html>
